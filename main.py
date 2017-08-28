1f5ca93fddca6340dc59b57db4b46415cf1c557e
Adam Allport <adam@allport.me.uk>
2017-08-28 21:04:21 +0100

    Main Template

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ Author: Adam Allport<adam@allport.me.uk>
 Description: Using Python to create a landscape silhouette
 Licence: GPLv3
 """
+
+
+def main():
+    """ Main program function, Draws silhouette"""
+
+
+if __name__ == "__main__":
+    main()
