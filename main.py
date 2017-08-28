ba821321cd87007f22c39eff176ffed653c19c71
Adam Allport <adam@allport.me.uk>
2017-08-28 21:47:58 +0100

    turtle setup code

--- main.py
+++ main.py
@@ -13,6 +13,13 @@ def main():
     """ Main program function, Draws silhouette"""
     charles = turtle.Turtle()
     screen = charles.screen
+    charles.penup()
+    charles.goto(-screen.window_width() / 2, -screen.window_height() / 2)
+    charles.setheading(0)
+    charles.pendown()
+    charles.speed(100)
+
+    turtle.done()
 
 
 if __name__ == "__main__":
