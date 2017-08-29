824b6329dceadeb5296ba93a877af60c3b17af81
Adam Allport <adam@allport.me.uk>
2017-08-29 08:54:53 +0100

    Created draw peak, contains some testing code

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ Licence: GPLv3
 """
 
 import turtle
+
 import units
 
 
@@ -17,9 +18,21 @@ def main():
     charles.goto(-screen.window_width() / 2, -screen.window_height() / 2)
     charles.setheading(0)
     charles.pendown()
-    charles.speed(100)
-
+    charles.speed(1000)
+    for i in range(units.wu2px(screen, 45)):
+        draw_peak(charles, units.vu2px(screen, 10))
     turtle.done()
+
+
+def draw_peak(drawer, height):
+    """ Draws a line with height """
+    drawer.left(90)
+    drawer.forward(height)
+    drawer.right(90)
+    drawer.forward(1)
+    drawer.right(90)
+    drawer.forward(height)
+    drawer.left(90)
 
 
 if __name__ == "__main__":
--- units.py
+++ units.py
@@ -9,9 +9,9 @@ Licence: GPLv3
 
 def vu2px(screen, units):
     """ Returns the px's for a vertical unit (25ths of height)"""
-    return (screen.window_height() / 25) * units
+    return int((screen.window_height() / 50) * units)
 
 
 def wu2px(screen, units):
     """ Returns the px's for a vertical unit (50ths of width)"""
-    return (screen.window_width() / 50) * units
+    return int((screen.window_width() / 50) * units)
